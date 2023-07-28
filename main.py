62ab2f387b3ef1d59d5bdb4c91e1b65775b061ac
Daniel Palma <danivgy@gmail.com>
2023-07-28 10:46:22 +0100

    use default chrome

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ load_dotenv()
 
 
 def get_driver():
+    print(
+        f"Installing chromedriver version {chromedriver_autoinstaller.get_chrome_version()}"
+    )
     chromedriver_autoinstaller.install()
 
     chrome_options = webdriver.ChromeOptions()
@@ -25,6 +28,7 @@ def get_driver():
     for option in options:
         chrome_options.add_argument(option)
 
+    print("Starting Chrome driver")
     driver = webdriver.Chrome(options=chrome_options)
     return driver
 
@@ -39,10 +43,13 @@ def login(driver):
     password.send_keys(os.getenv("TC_PASSWORD"))
 
     form = driver.find_element(by="name", value="loginform")
+    print(f"Submitting form: {form}")
     form.submit()
+    print("Submitted form")
 
 
 def feed(driver):
+    print(f"Loading feed page")
     driver.get("https://teveclub.hu/myteve.pet")
     try:
         _feed = driver.find_element(
@@ -50,11 +57,13 @@ def feed(driver):
             value='//*[@id="content ize"]/tbody/tr/td/table/tbody/tr[3]/td[1]/center/div/form/input',
         )
         _feed.click()
+        print(f"Clicked feed button")
     except NoSuchElementException:
         pass
 
 
 def learn(driver):
+    print(f"Loading learn page")
     driver.get("https://teveclub.hu/tanit.pet")
     try:
         _learn = driver.find_element(
@@ -62,6 +71,7 @@ def learn(driver):
             value='//*[@id="content ize"]/tbody/tr/td/table/tbody/tr[1]/td/font/b/div/form[1]/div/input',
         )
         _learn.click()
+        print(f"Clicked learn button")
     except NoSuchElementException:
         pass
 
@@ -69,8 +79,8 @@ def learn(driver):
 def main():
     driver = get_driver()
     login(driver)
-    # feed(driver)
-    # learn(driver)
+    feed(driver)
+    learn(driver)
 
 
 if __name__ == "__main__":
